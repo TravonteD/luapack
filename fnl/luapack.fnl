145e5b19bc8d237b55baf6013baff778a399262d
TravonteD <tman1300@aol.com>
2020-12-15 06:50:59 -0500

    Rewrite in fennel (#1)
    * add fennel version
    
    * refactor repeated code
    
    * add make file and compiled lua
    
    * Add makefile and fix clean function
    
    * fix the redraw function
    
    * fix run_cmd
    
    * fix install
    
    * break out openbuffer
    
    * update install method
    
    * Update
    
    * update displaylines

---
+++ fnl/luapack.fnl
@@ -0,0 +1,117 @@
+(let [Luapack {:plugins {}
+               :plugin_dir (string.format "%s/.local/share/nvim/site/pack/luapack/opt/" (os.getenv "HOME"))}
+      buffer (vim.api.nvim_create_buf false true)
+      statuses {}
+      jobs {}]
+  (var status_count 0)
+
+  (fn ensure_plugin_dir []
+    (if (= (vim.fn.isdirectory Luapack.plugin_dir) 0)
+      (vim.fn.mkdir Luapack.plugin_dir "p")))
+
+  (fn installed_plugins [] 
+    (vim.fn.readdir Luapack.plugin_dir))
+
+  (fn get_repo_name [str]
+    (str:gsub ".*/(.*)" "%1"))
+
+  (fn get_needed_plugins []
+    (let [installed_plugins (installed_plugins)
+          plugin_list {}]
+      (each [_ plugin (ipairs Luapack.plugins)]
+        (var installed? false)
+        (each [_ x (ipairs installed_plugins)]
+          (if (= x (get_repo_name plugin))
+                 (set installed? true)))
+        (if installed?
+          (tset plugin_list plugin true)
+          (tset plugin_list plugin false)))
+      plugin_list))
+
+  (fn redraw []
+    (let [lines []]
+      (each [plugin status (pairs statuses)]
+        (match status
+          "installed" (table.insert lines (string.format "Installing %s...already installed" plugin))
+          "removing" (table.insert lines (string.format "Removing %s" plugin))
+          "updating" (table.insert lines (string.format "Updating %s" plugin))
+          "installing" (table.insert lines (string.format "Installing %s" plugin))
+          "removing_done" (table.insert lines (string.format "Removing %s...done" plugin))
+          "updating_done" (table.insert lines (string.format "Updating %s...done" plugin))
+          "installing_done" (table.insert lines (string.format "Installing %s...done" plugin))
+          "error" (table.insert lines (string.format "Installing %s...error" plugin))))
+        (vim.api.nvim_buf_set_lines buffer 0 status_count false lines)))
+
+  (fn run_cmd [cmd name]
+    (let [job_id (vim.fn.jobstart
+                   cmd 
+                   {:on_exit (fn [id code _]
+                               (print (vim.inspect id))
+                                (let [name (. jobs id)]
+                                  (if (= code 0)
+                                    (tset statuses name (string.format "%s_done" (. statuses name)))
+                                    (tset statuses name "error"))
+                                  (redraw)))})]
+      (tset jobs job_id name)))
+
+  (fn load_helptags []
+    (each [_ plugin (ipairs (installed_plugins))]
+      (let [plugpath (.. Luapack.plugin_dir plugin)
+            dir (vim.fn.readdir plugpath)
+            doc_path (.. plugpath "/doc")]
+        (if (not= (vim.fn.index dir "doc") -1)
+          (if (= (vim.fn.index (vim.fn.readdir doc_path) "tags") -1)
+            (vim.cmd (string.format "helptags ++t %s" doc_path)))))))
+
+  (fn update_status [name status]
+    (tset statuses name status)
+    (set status_count (+ status_count 1))
+    (redraw))
+
+  (fn open_buffer []
+    (vim.cmd (string.format "vsplit | b%s" buffer)))
+
+  (fn install_plugin [plugin]
+    (update_status (get_repo_name plugin) "installing")
+    (let [shell_cmd (string.format "git clone https://github.com/%s %s" plugin (.. Luapack.plugin_dir (get_repo_name plugin)))]
+      (run_cmd shell_cmd (get_repo_name plugin))))
+
+  (fn update_plugin [plugin]
+    (update_status (get_repo_name plugin) "updating")
+    (let [shell_cmd (string.format "cd %s && git pull" (.. Luapack.plugin_dir (get_repo_name plugin)))]
+      (run_cmd shell_cmd (get_repo_name plugin))))
+
+  (fn Luapack.install []
+    (ensure_plugin_dir)
+    (open_buffer)
+    (each [plugin installed? (pairs (get_needed_plugins))]
+      (if installed?
+        (update_status (get_repo_name plugin) "installed")
+        (install_plugin plugin))))
+
+  (fn Luapack.update []
+    (open_buffer)
+    (each [plugin installed? (pairs (get_needed_plugins))]
+      (if installed?
+        (update_plugin plugin)
+        (install_plugin plugin))))
+  
+  (fn Luapack.clean []
+    (let [plugins_to_remove []]
+      (each [_ plugin (ipairs (installed_plugins))]
+        (var to_delete true)
+        (each [_ x (ipairs Luapack.plugins)]
+          (if (= plugin (get_repo_name x))
+            (set to_delete false)))
+        (if to_delete
+          (table.insert plugins_to_remove plugin)))
+      (open_buffer)
+      (each [_ plugin (ipairs plugins_to_remove)]
+        (update_status plugin "removing")
+        (let [shell_cmd (string.format "rm -fr %s" (.. Luapack.plugin_dir plugin))]
+          (run_cmd shell_cmd plugin)))))
+
+  (fn Luapack.load []
+    (each [_ plugin (ipairs (installed_plugins))]
+      (vim.cmd (string.format "packadd %s" plugin))))
+  Luapack)
--- lua/luapack.lua
+++ lua/luapack.lua
@@ -1,138 +1,137 @@
--- luacheck: globals vim
-local Luapack = {}
+local Luapack = {plugin_dir = string.format("%s/.local/share/nvim/site/pack/luapack/opt/", os.getenv("HOME")), plugins = {}}
 local buffer = vim.api.nvim_create_buf(false, true)
 local statuses = {}
 local jobs = {}
 local status_count = 0
-
-Luapack.plugins = {}
-
-Luapack.plugin_dir = ('%s/.local/share/nvim/site/pack/luapack/opt/'):format(os.getenv('HOME'))
-
 local function ensure_plugin_dir()
-  if vim.fn.isdirectory(Luapack.plugin_dir) == 0 then
-    vim.fn.mkdir(Luapack.plugin_dir, 'p')
+  if (vim.fn.isdirectory(Luapack.plugin_dir) == 0) then
+    return vim.fn.mkdir(Luapack.plugin_dir, "p")
   end
 end
-
 local function installed_plugins()
   return vim.fn.readdir(Luapack.plugin_dir)
 end
-
 local function get_repo_name(str)
-  return str:gsub('.*/(.*)', '%1')
+  return str:gsub(".*/(.*)", "%1")
 end
-
 local function get_needed_plugins()
-  local already_installed = installed_plugins()
-  local to_be_installed = {}
+  local installed_plugins0 = installed_plugins()
+  local plugin_list = {}
   for _, plugin in ipairs(Luapack.plugins) do
-    local needed = false
-    for _, x in ipairs(already_installed) do
-      if x == get_repo_name(plugin) then
-         needed = true
+    local installed_3f = false
+    for _0, x in ipairs(installed_plugins0) do
+      if (x == get_repo_name(plugin)) then
+        installed_3f = true
       end
     end
-    if not needed then
-      table.insert(to_be_installed, plugin)
+    if installed_3f then
+      plugin_list[plugin] = true
+    else
+      plugin_list[plugin] = false
     end
   end
-  return to_be_installed
+  return plugin_list
 end
-
 local function redraw()
   local lines = {}
   for plugin, status in pairs(statuses) do
-    if status == 'deleting' then
-      table.insert(lines, ('Deleting %s'):format(plugin))
-      goto continue
+    local _0_0 = status
+    if (_0_0 == "installed") then
+      table.insert(lines, string.format("Installing %s...already installed", plugin))
+    elseif (_0_0 == "removing") then
+      table.insert(lines, string.format("Removing %s", plugin))
+    elseif (_0_0 == "updating") then
+      table.insert(lines, string.format("Updating %s", plugin))
+    elseif (_0_0 == "installing") then
+      table.insert(lines, string.format("Installing %s", plugin))
+    elseif (_0_0 == "removing_done") then
+      table.insert(lines, string.format("Removing %s...done", plugin))
+    elseif (_0_0 == "updating_done") then
+      table.insert(lines, string.format("Updating %s...done", plugin))
+    elseif (_0_0 == "installing_done") then
+      table.insert(lines, string.format("Installing %s...done", plugin))
+    elseif (_0_0 == "error") then
+      table.insert(lines, string.format("Installing %s...error", plugin))
     end
-    if status == 'updating' then
-      table.insert(lines, ('Updating %s'):format(plugin))
-      goto continue
-    end
-    if status == 'downloading' then
-      table.insert(lines, ('Downloading %s'):format(plugin))
-      goto continue
-    end
-    if status == 'deleting_done' then
-      table.insert(lines, ('Deleting %s...done'):format(plugin))
-      goto continue
-    end
-    if status == 'updating_done' then
-      table.insert(lines, ('Updating %s...done'):format(plugin))
-      goto continue
-    end
-    if status == 'downloading_done' then
-      table.insert(lines, ('Downloading %s...done'):format(plugin))
-      goto continue
-    end
-    if status == 'error' then
-      table.insert(lines, ('Downloading %s...error'):format(plugin))
-      goto continue
-    end
-    :: continue ::
   end
-  vim.api.nvim_buf_set_lines(buffer, 0, status_count, false, lines)
+  return vim.api.nvim_buf_set_lines(buffer, 0, status_count, false, lines)
 end
-
 local function run_cmd(cmd, name)
-  local job_id = vim.fn.jobstart(cmd, {
-      on_exit = function(id, code, _)
-        local name = jobs[id]
-        if code == 0 then
-          statuses[name] = ('%s_done'):format(statuses[name])
-        else
-          statuses[name] = 'error'
-        end
-        redraw()
-      end
-  })
+  local job_id = nil
+  local function _0_(id, code, _)
+    print(vim.inspect(id))
+    local name0 = jobs[id]
+    if (code == 0) then
+      statuses[name0] = string.format("%s_done", statuses[name0])
+    else
+      statuses[name0] = "error"
+    end
+    return redraw()
+  end
+  job_id = vim.fn.jobstart(cmd, {on_exit = _0_})
   jobs[job_id] = name
+  return nil
 end
-
 local function load_helptags()
   for _, plugin in ipairs(installed_plugins()) do
-    local plugpath = Luapack.plugin_dir..plugin
+    local plugpath = (Luapack.plugin_dir .. plugin)
     local dir = vim.fn.readdir(plugpath)
-    if vim.fn.index(dir, 'doc') ~= -1 then
-      if vim.fn.index(vim.fn.readdir(plugpath..'/doc'), 'tags') == -1 then
-        vim.cmd(('helptags ++t %s'):format(plugpath..'/doc'))
+    local doc_path = (plugpath .. "/doc")
+    if (vim.fn.index(dir, "doc") ~= -1) then
+      if (vim.fn.index(vim.fn.readdir(doc_path), "tags") == -1) then
+        vim.cmd(string.format("helptags ++t %s", doc_path))
       end
     end
   end
+  return nil
 end
-
+local function update_status(name, status)
+  statuses[name] = status
+  status_count = (status_count + 1)
+  return redraw()
+end
+local function open_buffer()
+  return vim.cmd(string.format("vsplit | b%s", buffer))
+end
+local function install_plugin(plugin)
+  update_status(get_repo_name(plugin), "installing")
+  local shell_cmd = string.format("git clone https://github.com/%s %s", plugin, (Luapack.plugin_dir .. get_repo_name(plugin)))
+  return run_cmd(shell_cmd, get_repo_name(plugin))
+end
+local function update_plugin(plugin)
+  update_status(get_repo_name(plugin), "updating")
+  local shell_cmd = string.format("cd %s && git pull", (Luapack.plugin_dir .. get_repo_name(plugin)))
+  return run_cmd(shell_cmd, get_repo_name(plugin))
+end
 Luapack.install = function()
   ensure_plugin_dir()
-  vim.cmd(([[vsplit | b%s]]):format(buffer))
-  for _, x in ipairs(get_needed_plugins()) do
-      statuses[get_repo_name(x)] = 'downloading'
-      status_count = status_count + 1
-      redraw()
-      local shell_cmd = ('git clone https://github.com/%s %s'):format(x, Luapack.plugin_dir..get_repo_name(x))
-      run_cmd(shell_cmd, get_repo_name(x))
+  open_buffer()
+  for plugin, installed_3f in pairs(get_needed_plugins()) do
+    if installed_3f then
+      update_status(get_repo_name(plugin), "installed")
+    else
+      install_plugin(plugin)
+    end
   end
+  return nil
 end
-
 Luapack.update = function()
-  vim.cmd(([[vsplit | b%s]]):format(buffer))
-  for _, x in ipairs(installed_plugins()) do
-      statuses[get_repo_name(x)] = 'updating'
-      status_count = status_count + 1
-      redraw()
-      local shell_cmd = ('cd %s && git pull'):format(Luapack.plugin_dir..get_repo_name(x))
-      run_cmd(shell_cmd, get_repo_name(x))
+  open_buffer()
+  for plugin, installed_3f in pairs(get_needed_plugins()) do
+    if installed_3f then
+      update_plugin(plugin)
+    else
+      install_plugin(plugin)
+    end
   end
-  load_helptags()
+  return nil
 end
-
 Luapack.clean = function()
   local plugins_to_remove = {}
   for _, plugin in ipairs(installed_plugins()) do
     local to_delete = true
-    for _, x in ipairs(plugins) do
-      if plugin == get_repo_name(x) then
+    for _0, x in ipairs(Luapack.plugins) do
+      if (plugin == get_repo_name(x)) then
         to_delete = false
       end
     end
@@ -140,22 +139,18 @@ Luapack.clean = function()
       table.insert(plugins_to_remove, plugin)
     end
   end
-
-  vim.cmd(([[vsplit | b%s]]):format(buffer))
+  open_buffer()
   for _, plugin in ipairs(plugins_to_remove) do
-    statuses[plugin] = 'deleting'
-    status_count = status_count + 1
-    redraw()
-    local shell_cmd = ('rm -fr %s'):format(Luapack.plugin_dir..plugin)
+    update_status(plugin, "removing")
+    local shell_cmd = string.format("rm -fr %s", (Luapack.plugin_dir .. plugin))
     run_cmd(shell_cmd, plugin)
   end
-  load_helptags()
+  return nil
 end
-
 Luapack.load = function()
   for _, plugin in ipairs(installed_plugins()) do
-    vim.cmd(('packadd %s'):format(plugin))
+    vim.cmd(string.format("packadd %s", plugin))
   end
+  return nil
 end
-
 return Luapack
